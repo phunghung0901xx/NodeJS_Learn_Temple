4025f14d6322c675739f8b3a444856ffb6cbd27a
phunghung0901xx <phunghung0901xx@gmail.com>
2023-01-30 22:47:15 +0700

    add feature models

--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,10 @@ const router = require("express").Router()
 router.get("/usertest", (req, res) => {
     res.send("user test is succesfull")
 })
+
+router.post("/userposttest", (req,res) => {
+    const username=req.body.username
+    console.log(username)
+    res.send("your username is:"+username)
+})
 module.exports = router
