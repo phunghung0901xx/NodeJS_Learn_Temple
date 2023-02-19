8382c793032aad9203799bcbfa6eb603246e144b
phunghung0901xx <phunghung0901xx@gmail.com>
2023-02-19 14:58:55 +0700

    19/2

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const router = require("express").Router()
+const User = require('../models/User')
+const CryptoJS = require("crypto-js")
+
+//REGISTER
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+    });
+    try {
+        const savedUser = await newUser.save();
+        res.status(200).json(savedUser)
+        console.log(saveUser)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+module.exports = router
